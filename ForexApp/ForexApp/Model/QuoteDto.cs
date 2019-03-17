c3375081a90a6620b0b20d2945f3878c8a5b9f6e
Thomas Galliker <thomas@bluewin.ch>
2019-03-17 17:32:22 +0100

    Add comment

--- ForexApp/ForexApp/Model/QuoteDto.cs
+++ ForexApp/ForexApp/Model/QuoteDto.cs
@@ -2,7 +2,7 @@ namespace ForexApp.Model
 {
     public class QuoteDto
     {
-        //{"symbol":"EURUSD","price":1.24308,"bid":1.24288,"ask":1.24328,"timestamp":1517056850}
+        //JSON String: {"symbol":"EURUSD","price":1.24308,"bid":1.24288,"ask":1.24328,"timestamp":1517056850}
 
         public string Symbol { get; set; }
 
