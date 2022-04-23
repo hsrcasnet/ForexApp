53c3b14f5cf0c35601745c755363744a94d70cbb
Thomas Galliker <thomas@bluewin.ch>
2022-04-23 15:33:16 +0200

    Code cleanup

--- ForexApp/ForexApp/Views/Cells/QuoteItemViewCell.xaml.cs
+++ ForexApp/ForexApp/Views/Cells/QuoteItemViewCell.xaml.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -14,7 +9,7 @@ namespace ForexApp.Views.Cells
     {
         public QuoteItemViewCell()
         {
-            InitializeComponent();
+            this.InitializeComponent();
         }
     }
 }
