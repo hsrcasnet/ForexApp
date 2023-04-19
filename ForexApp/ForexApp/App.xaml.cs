73ba7e16d89650de4f7088a3fc42d327f9e175c7
Thomas Galliker <thomas@bluewin.ch>
2023-04-19 21:00:16 +0200

    Resolve ITranslationProvider from DI

--- ForexApp/ForexApp/App.xaml.cs
+++ ForexApp/ForexApp/App.xaml.cs
@@ -50,7 +50,7 @@ namespace ForexApp
 
             TranslateExtension.Init(
                 () => base.Container.Resolve<ILocalizer>(),
-                () => ResxTranslationProvider.Instance);
+                () => base.Container.Resolve<ITranslationProvider>());
         }
 
 
